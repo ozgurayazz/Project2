6b500242a75cb12b46fac3111c4e346ff40a2866
User <User@OZGURPC>
2023-11-29 22:00:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Proje2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.sayıyı giriniz");
+            byte sayi1 = byte.Parse(Console.ReadLine());
+            Console.WriteLine("2.sayıyı giriniz");
+            byte sayi2 = byte.Parse(Console.ReadLine());
+            string sonuc = sayi1 > sayi2 ? "birinci sayı büyüktür" : "2.sayı büyüktür ya da sayılar eşittir";
+            Console.WriteLine(sonuc);
+            Console.ReadKey();
+        }
+    }
+}
